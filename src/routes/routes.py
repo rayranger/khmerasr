3d0656d5d951c692181ae67eec87b2e8793369cc
raychannudam <channudamray2002.rcu@gmail.com>
2022-09-09 15:32:32 +0700

    testing route with controller

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -2,6 +2,9 @@ from crypt import methods
 from src import app
 from flask import render_template
 from src.forms.sing_in_form import SigninForm
+from src.controllers import user_controller
+
+userController = user_controller.UserController()
 
 @app.route('/')
 def home_page():
@@ -11,3 +14,11 @@ def home_page():
 def signin_page():
     sign_in_form = SigninForm()
     return render_template('sign-in.html', sign_in_form = sign_in_form)
+
+# @app.route('/create-user')
+# def create_page():
+#     user = userController.create_user(username='punsolita', email='punsolita@gmail.com', password='1234')
+#     if user:
+#         return 'Done'
+#     else:
+#         return 'Undone'
