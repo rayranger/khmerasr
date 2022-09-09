a0bf08b2f68247f11829f63246bc4e1928fcf771
raychannudam <channudamray2002.rcu@gmail.com>
2022-09-09 15:40:08 +0700

    created runscript

--- migration.py
+++ migration.py
@@ -1,4 +1,3 @@
 from src import db
-# from src.models import models
 from src.models import record, record_category, role, speaker, user, record_config, task
 db.create_all()
---
+++ runscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python3 migration.py
+python3 seeders.py
+python3 run.py
+exit
