d4299140eaae3718eff3f1a65d1360bf449b2f0e
raychannudam <channudamray2002.rcu@gmail.com>
2022-10-06 15:38:28 +0700

    completed_main_recording_feature

--- src/controllers/audio_controller.py
+++ src/controllers/audio_controller.py
@@ -1,98 +1,58 @@
+from this import d
 from src.models import audio
 from datetime import datetime
+import soundfile
 from src import db
 import os.path
-import wave
 import pyaudio
 
 class AudioController():
 
     FRAMES_PER_BUFFER = 0
-    FORMAT = 0
+    FORMAT = pyaudio.paInt16
     CHANNEL = 0
     RATE = 0
     FILETYPE = ''
     DURATION = 0
-    AUDIO_DIR = "storage/audios"
+    AUDIO_DIR = "src/static/storage/audios/recordings"
 
-    def getRecordConfig(self, frame_per_buffer, format, channel, rate, filetype, duration):
+    def getRecordConfig(self, frame_per_buffer, channel, rate, filetype, duration):
         self.FRAMES_PER_BUFFER = frame_per_buffer
-        self.FORMAT = format
         self.CHANNEL = channel
         self.RATE = rate
         self.FILETYPE = filetype
         self.DURATION = duration
 
+    def set_audio_filename(self, username):
+        return f'{username}_{str(datetime.now().strftime("%d%m%Y_%H%M%S"))}.ogg'
 
     def is_existed(self, data):
-        req = audio.Record.query.filter_by(id=data).first()
+        req = audio.Audio.query.filter_by(id=data).first()
         if req:
             return req
         else:
             return None
 
-    def create_record(self, username):
-        FILE_NAME = f'{username}_{str(datetime.now().strftime("%d%m%Y_%H%M%S"))}.{self.FILETYPE}'
-        FILE_PATH = os.path.join(self.AUDIO_DIR, FILE_NAME)
-        print(FILE_PATH)
-        p = pyaudio.PyAudio()
-        print("Start stream")
-        stream = p.open(
-            format=self.FORMAT,
-            channels=self.CHANNELS,
-            rate=self.RATE,
-            input=True,
-            frames_per_buffer=self.FRAMES_PER_BUFFER,
-            
-        )
-        # seconds = 3
-        frames = []
-        for i in range(0, int(self.RATE/self.FRAMES_PER_BUFFER*self.DURATION)):
-            data = stream.read(self.FRAMES_PER_BUFFER)
-            frames.append(data)
-
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-        obj = wave.open(FILE_PATH, "wb")
-        obj.setnchannels(self.CHANNELS)
-        obj.setsampwidth(p.get_sample_size(self.FORMAT))
-        obj.setframerate(self.RATE)
-        obj.writeframes(b"".join(frames))
-        obj.close()
-        return FILE_NAME
-
-    def create_audio(self, filename, category_id, speaker_id):
+    def create_audio(self, filename, recording_id, speaker_id):
         file_path = os.path.join(self.AUDIO_DIR, filename)
+        print(file_path)
         if self.is_existed(data=filename):
             return None
         else:
-            obj = wave.open(file_path, "rb")
-            filetype="wav"
-            filesize = obj.getnframes()
-            channel = obj.getnchannels()
-            sample_framerate = obj.getframerate()
-            sample_frame = filesize
-            total_frame = obj.readframes(-1)
-            duration = sample_framerate / sample_frame
-            new_record = audio.Record(
+            # data, sample_rate = soundfile.read(file_path)
+            new_audio = audio.Audio(
                 filename=filename,
-                filetype=filetype,
-                filesize=filesize,
-                channel=channel,
-                sample_framerate=sample_framerate,
-                sample_frame=sample_frame,
-                total_frame=total_frame,
-                duration=duration,
+                # sample_rate = sample_rate,
                 speaker_id = speaker_id,
-                category_id=category_id
+                recording_id=recording_id
             )
-            db.session.add(new_record)
+            # print(new_audio)
+            db.session.add(new_audio)
             db.session.commit()
-            return new_record
+            return new_audio
+            return None
     
-    def delete_record(self, id):
+    def delete_audio(self, id):
         recordFile = self.is_existed(data=id)
         if recordFile:
             db.session.delete(recordFile)
@@ -101,13 +61,18 @@ class AudioController():
         else:
             return None
     
-    def get_all_record(self):
+    def get_all_audio(self):
         records = audio.Audio.query.all()
         recordList = []
         for recordItem in records:
             recordList.append(recordItem)
         return recordList
-
     
+    def getAllAudioByUser(self, speakerId):
+        audios = audio.Audio.query.filter_by(speaker_id=speakerId)
+        audioList = []
+        for audioItem in audios:
+            audioList.append(audioItem)
+        return audioList
         
         
