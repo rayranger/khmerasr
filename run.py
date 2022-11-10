3e4cd40ad40a632959a614317e9dcd9a0dc7295a
raychannudam <channudamray2002.rcu@gmail.com>
2022-11-10 15:25:00 +0700

    edited task page

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ from waitress import serve
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # app.run(debug=True)
-        serve(app, port='5000', host='0.0.0.0', threads=2)
+        app.run(debug=True)
+        # serve(app, port='5000', host='0.0.0.0', threads=2)
